a03abef8d2c3fab899a34cae90acb9727d772b01
Vishnu <vishnusr01@gmail.com>
2022-03-05 17:14:36 +0530

    Add lower case command handling

--- src/main/java/com/console/consoledrawingapp/commands/CommandFactory.java
+++ src/main/java/com/console/consoledrawingapp/commands/CommandFactory.java
@@ -1,9 +1,8 @@
-package com.console.consoledrawingapp;
+package com.console.consoledrawingapp.commands;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-import com.console.consoledrawingapp.commands.Command;
 import com.console.consoledrawingapp.commands.impl.CanvasCommand;
 import com.console.consoledrawingapp.commands.impl.LineCommand;
 import com.console.consoledrawingapp.commands.impl.RectangleCommand;
@@ -15,15 +14,15 @@ public class CommandFactory {
     
     public static Command getCommand(String userInput, Scanner sc) {
         String[] input = userInput.split(" ");
-        String mainCommand = input[0];
+        String mainCommand = input[0].toLowerCase();
         String[] remainingCommands = Arrays.copyOfRange(input, 1, input.length);
 
         switch(mainCommand) {
-            case "C": return new CanvasCommand(remainingCommands);
-            case "L": return new LineCommand(remainingCommands);
-            case "R": return new RectangleCommand(remainingCommands);
-            case "B": return new FillAreaCommand(remainingCommands);
-            case "Q": return new QuitCommand(sc);
+            case "c": return new CanvasCommand(remainingCommands);
+            case "l": return new LineCommand(remainingCommands);
+            case "r": return new RectangleCommand(remainingCommands);
+            case "b": return new FillAreaCommand(remainingCommands);
+            case "q": return new QuitCommand(sc);
             default:
                 throw new InvalidCommandException("Please enter correct commands !");
         }
